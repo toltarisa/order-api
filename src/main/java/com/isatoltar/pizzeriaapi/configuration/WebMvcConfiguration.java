5bd5d49b71b129552c721021b4898c600381bb38
toltarisa <isatoltar@gmail.com>
2022-08-09 23:27:42 +0300

    swagger 2 added

---
+++ src/main/java/com/isatoltar/pizzeriaapi/configuration/WebMvcConfiguration.java
@@ -0,0 +1,18 @@
+package com.isatoltar.pizzeriaapi.configuration;
+
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@EnableWebMvc
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+}
